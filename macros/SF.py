7b8c5c848b9e84ac218404d770b474c262d87c2c
Mikhail Makouski <makouski@gmail.com>
2014-01-29 11:31:07 -0600

    scale factros update

--- macros/SF.py
+++ macros/SF.py
@@ -1,6 +1,6 @@
 
 luminosity = 19.63*1000
-gSF = luminosity
+gSF = luminosity * 0.97
 
 TTJets1l_num        = 24849110 #
 TTJets2l_num        = 12086717 #
@@ -12,21 +12,22 @@ WJets_num      = 57709905 #
 ZJets_num      = 30458871 #
 
 #ZZ_num            = 9799908 #AODSIM inclusive?
+
 # 2e2mu + 2e2tau + 2mu2tau + 4e + 4mu + 4tau
-#ZZ_2e2mu_num       = 1497445
-#ZZ_2e2tau_num      = 823911
-#ZZ_2mu2tau_num     = 823922
-#ZZ_4e_num          = 1499093
-#ZZ_4mu_num         = 1499064
-#ZZ_4tau_num        = 824466
+ZZ_2e2mu_num       = 1497445
+ZZ_2e2tau_num      = 823911
+ZZ_2mu2tau_num     = 823922
+ZZ_4e_num          = 1499093
+ZZ_4mu_num         = 1499064
+ZZ_4tau_num        = 824466
 
 #WW_num            = 10000431 #AODSIM
-#WW_2l2nu_num       = 1903235 #
+WW_2l2nu_num       = 1933120 # 1903235 #
 
 #WZ_num            = 10000283 #AODSIM
 #WZ_num            = 5233969 # 
-#WZ_3lnu_num       = 2017979 #
-#WZ_2l2q_num       = 3215990 #
+WZ_3lnu_num       = 2017979 #
+WZ_2l2q_num       = 3215990 #
 
 Wgamma_num        = 4877150 #
 
@@ -58,30 +59,30 @@ SingTopS_num      =  259961 #
 SingTopbarS_num   =  139974 #
 
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/QGC
-Zgamma_xs         = 132.6 # PREP
-Wgamma_xs         = 461.6 # PREP
-WWgamma_xs        = 0.528 # PREP
+Zgamma_xs         = 159.12 # 132.6 # PREP
+Wgamma_xs         = 553.92 # 461.6 # PREP
+WWgamma_xs        = 1.44 #0.528 # PREP
 
 # https://twiki.cern.ch/twiki/bin/viewauth/CMS/StandardModelCrossSectionsat8TeV
 
-#Top_xs            = 227 #CMS measurement
-TTJets1l_xs       = 99.44 # 227*0.676*(1-0.676)*2
-TTJets2l_xs       = 23.83 # 227*(1-0.676)*(1-0.676)
+#Top_xs            = 239 # new measurement #227 #CMS measurement
+TTJets1l_xs       = 104.7 # 227*0.676*(1-0.676)*2
+TTJets2l_xs       = 25.09 # 227*(1-0.676)*(1-0.676)
 TTgamma_xs        = 0.9081 * 2  # https://twiki.cern.ch/twiki/bin/view/CMS/WhizardMCTeeTeeGamma
-WJets_xs          = 35640.0 # CMS measurement. was: 36257.0  # 
+WJets_xs          = 36257.0 # 35640.0 # CMS measurement. was: 36257.0  # 
 ZJets_xs          = 3350.0 # CMS measurement. was: 3533.0 # 3503.71 #
 
 #WZ_xs             = 33.21 # inclusive
-WZ_3lnu_xs        = 0.8674 # PREP
-WZ_2l2q_xs        = 1.755  # PREP
+WZ_3lnu_xs        = 1.057 # 0.8674 # PREP
+WZ_2l2q_xs        = 5.995 # 1.755  # PREP
 
-#ZZ_xs             = 8.06 # inclusive
-ZZ_2e2mu_xs       = 0.018 #??? 0.1767 # PREP
-ZZ_2e2tau_xs      = 0.018 #??? 0.1767 # PREP
-ZZ_2mu2tau_xs     = 0.018 #??? 0.1767 # PREP
-ZZ_4e_xs          = 0.009 #??? 0.07691 # PREP
-ZZ_4mu_xs         = 0.009 #??? 0.07691 # PREP
-ZZ_4tau_xs        = 0.009 #??? 0.07691 # PREP
+#ZZ_xs             = 8.06 # inclusive #  4l 0.009?, 2l2l 0.018?
+ZZ_2e2mu_xs       = 0.1767 # PREP
+ZZ_2e2tau_xs      = 0.1767 # PREP
+ZZ_2mu2tau_xs     = 0.1767 # PREP
+ZZ_4e_xs          = 0.07691 # PREP
+ZZ_4mu_xs         = 0.07691 # PREP
+ZZ_4tau_xs        = 0.07691 # PREP
 
 # NLO? 5.995 # 4.7 PREP this is 2l2nu, inclusive is 54.8
 # 69.9 CMS measurement
