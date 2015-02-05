a8cb7d42c89acde23eb2a808f74aa93afc530c13
Mikhail Makouski <makouski@gmail.com>
2015-02-05 11:44:02 -0600

    propagating stat errors from MC to the likelihood

--- macros/signal_finder.py
+++ macros/signal_finder.py
@@ -32,58 +32,66 @@ def readSamples(suffix):
 	var = 'MET'
 	samplnames = ['TTGamma', 'TTJets', 'Vgamma', 'SingleTop', 'WJets', 'ZJets', 'Diboson', 'QCD']
 	samples = {}
-	errors = {}
 	for n in samplnames:
 		if n=='QCD' and (suffix=='signal' or suffix=='electron'):
-			samples[n] = 0.0
-			errors[n] = 0.0
+			samples[n] = (0.0,0.0)
 			continue
 		print 'getting counts for ',n,suffix
 		if n=='QCD':
 			int,err = integral(n+'_'+var)
 		else:
 			int,err = integral(n+'_'+suffix+'_'+var)
-		samples[n] = int
-		errors[n] = err
+		samples[n] = (int,err)
 		
-	# deal with errors later
 	return samples
 
 def Negamma(pho,ele,fake, ttgSF, VgSF, jgSF):
 	sum = 0.0
+	err2 = 0.0
 	for sampln in pho:
-		npho = pho[sampln]
+		npho,nphoErr = pho[sampln]
 		if sampln == 'TTGamma':
 			npho *= ttgSF
+			nphoErr *= ttgSF
 		if sampln == 'Vgamma':
 			npho *= VgSF
+			nphoErr *= VgSF
 		sum += npho
+		err2 += nphoErr**2
 	
 	for sampln in ele:
-		nele = ele[sampln]
+		nele,neleErr = ele[sampln]
 		nele *= eleFakeSF
+		neleErr *= eleFakeSF
 		if sampln == 'TTGamma':
 			nele *= ttgSF
+			neleErr *= ttgSF
 		if sampln == 'Vgamma':
 			nele *= VgSF
+			neleErr *= VgSF
 		sum += nele
+		err2 += neleErr**2
 	
-	return sum
+	return sum,err2**0.5
 
 def NallMC(pho,ele,fake, ttgSF, VgSF, jgSF):
 	# all MC is egamma plus fakes
-	sum = Negamma(pho,ele,fake, ttgSF, VgSF, jgSF)
-	
+	sum,err = Negamma(pho,ele,fake, ttgSF, VgSF, jgSF)
+	err2 = err**2
 	for sampln in fake:
-		nfake = fake[sampln]
+		nfake,nfakeErr = fake[sampln]
 		nfake *= jgSF
+		nfakeErr *= jgSF
 		if sampln == 'TTGamma':
 			nfake *= ttgSF
+			nfakeErr *= ttgSF
 		if sampln == 'Vgamma':
 			nfake *= VgSF
+			nfakeErr *= VgSF
 		sum += nfake
+		err2 += nfakeErr**2
 	
-	return sum
+	return sum,err2**0.5
 
 
 def Ntop(pho,ele,fake, ttgSF, VgSF, jgSF):
@@ -100,13 +108,17 @@ def Ntop(pho,ele,fake, ttgSF, VgSF, jgSF):
 
 
 def getChiSq(pho,ele,fake, ttgSF, VgSF, jgSF):
-	allmc = NallMC(pho,ele,fake, ttgSF, VgSF, jgSF)
-	top = Ntop(pho,ele,fake, ttgSF, VgSF, jgSF)
-	egamma = Negamma(pho,ele,fake, ttgSF, VgSF, jgSF)
+	allmc,allmcErr = NallMC(pho,ele,fake, ttgSF, VgSF, jgSF)
+	top,topErr = Ntop(pho,ele,fake, ttgSF, VgSF, jgSF)
+	egamma,egammaErr = Negamma(pho,ele,fake, ttgSF, VgSF, jgSF)
 	cs = 0.0
-	cs += (allmc - Ndata)**2 / NdataErr**2
-	cs += (egamma/allmc - photnPurity)**2 / photnPurityErr**2
-	cs += (top/allmc - M3_photon_topFrac)**2 / M3_photon_topFracErr**2
+	cs += (allmc - Ndata)**2 / (NdataErr**2 + allmcErr**2)
+	
+	mcPurErr = (egamma/allmc) * ((egammaErr/egamma)**2 + (allmcErr/allmc)**2)**0.5 
+	cs += (egamma/allmc - photnPurity)**2 / (photnPurityErr**2 + mcPurErr**2)
+	
+	mcTopErr = (top/allmc) * ((topErr/top)**2 + (allmcErr/allmc)**2)**0.5
+	cs += (top/allmc - M3_photon_topFrac)**2 / (M3_photon_topFracErr**2 + mcTopErr**2)
 	return cs
 
 def getLikelihood(pho,ele,fake, ttgSF, VgSF, jgSF):
@@ -125,21 +137,21 @@ def calculateTTGamma():
 	ele = readSamples('electron')
 	fake = readSamples('fake')
 	
-	ttghist = ROOT.TH1F('ttghist','Marginalized Likelihood',140, 0.2+0.005,1.6+0.005)
-	vghist = ROOT.TH1F('vghist','Marginalized Likelihood',130, -2.0+0.025,4.5+0.025)
-	jghist = ROOT.TH1F('jghist','Marginalized Likelihood',120, 0.6+0.005,1.8+0.005)
+	ttghist = ROOT.TH1F('ttghist','Marginalized Likelihood', 141, 0.2-0.005, 1.6+0.005)
+	vghist = ROOT.TH1F('vghist','Marginalized Likelihood', 61, 0-0.025, 3.0+0.025)
+	jghist = ROOT.TH1F('jghist','Marginalized Likelihood', 131, 0.5-0.005, 1.8+0.005)
 	maxlk = -1.0
 	bestttgSF = -1.0
 	bestVgSF = -1.0
 	bestjgSF = -1.0
 	
 	for ttgSF in seq(0.2, 1.6, 0.01):
-		for VgSF in seq(-2.0, 4.5, 0.05):
-			for jgSF in seq(0.6, 1.8, 0.01):
+		for VgSF in seq(0.0, 3.0, 0.05):
+			for jgSF in seq(0.5, 1.8, 0.01):
 				lk = getLikelihood(pho,ele,fake,ttgSF, VgSF, jgSF)
-				ttghist.Fill(ttgSF,lk*0.001)
+				ttghist.Fill(ttgSF,lk*0.0005)
 				vghist.Fill(VgSF,lk*0.0001)
-				jghist.Fill(jgSF,lk*0.001)
+				jghist.Fill(jgSF,lk*0.0005)
 				if lk > maxlk:
 					maxlk = lk
 					bestttgSF = ttgSF
@@ -150,9 +162,9 @@ def calculateTTGamma():
 	print 'best ttgSF',bestttgSF
 	print 'best VgSF',bestVgSF
 	print 'best jgSF',bestjgSF
-	b_allmc = NallMC(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
-	b_top = Ntop(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
-	b_egamma = Negamma(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
+	b_allmc,allmcErr = NallMC(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
+	b_top,topErr = Ntop(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
+	b_egamma,egammaErr = Negamma(pho,ele,fake, bestttgSF, bestVgSF, bestjgSF)
 	print 'total number of MC events ',b_allmc, '  Data:',Ndata
 	print 'photon purity in MC ',b_egamma/b_allmc, '  template fit:',photnPurity
 	print 'top fraction in MC ',b_top/b_allmc, '  M3 fit:',M3_photon_topFrac
@@ -168,6 +180,7 @@ def calculateTTGamma():
 	
 	vghist.Draw()
 	vghist.GetXaxis().SetTitle('Vgamma Scale Factor')
+	vghist.SetMinimum(0.0)
 	vghist.Fit('gaus')
 	ccc.SaveAs('Vgamma_SF_Lkhood.png')
 	
@@ -176,8 +189,8 @@ def calculateTTGamma():
 	jghist.Fit('gaus')
 	ccc.SaveAs('jet_gamma_SF_Lkhood.png')
 	
-	ttgammaSig = bestttgSF*pho['TTGamma']
-	ttgammaSigErr = bestttgSFErr*pho['TTGamma']
+	ttgammaSig = bestttgSF*pho['TTGamma'][0]
+	ttgammaSigErr = bestttgSFErr*pho['TTGamma'][0]
 	print 'number of signal events', ttgammaSig, ' +/-',ttgammaSigErr
 	return ttgammaSig,ttgammaSigErr
 
